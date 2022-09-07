027f09e772156c88af328890e86d0a805ab0cef4
RaoulBumbasiJr <100057772+RaoulBumbasiJr@users.noreply.github.com>
2022-09-06 17:18:00 -0700

    Add files via upload

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -4,11 +4,22 @@ import "./home.css"
 export default function home() {
   return (
     <div className="home" id="home">
+        
         <div className="intro">
           hello, my name is raoul bumbasi jr
         </div>
+        
         <div className="text">
           i can code in javascript
+        </div>
+
+          <div className="photos">
+            <div className="block">1</div>
+            <div className="block">2</div>
+            <div className="block">3</div>
+            <div className="block">4</div>
+            <div className="block">5</div>
+          <div className="block">6</div>
         </div>
     </div>
   )
