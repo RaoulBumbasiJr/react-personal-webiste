25396929331e3db47abcba9492a3e8809883093d
RaoulBumbasiJr <100057772+RaoulBumbasiJr@users.noreply.github.com>
2022-10-01 22:33:00 -0700

    Add files via upload

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -14,12 +14,12 @@ export default function home() {
         </div>
 
           <div className="photos">
-            <div className="block">1</div>
+            <div className="block"><img src="" alt="" /></div>
             <div className="block">2</div>
             <div className="block">3</div>
             <div className="block">4</div>
             <div className="block">5</div>
-          <div className="block">6</div>
+            <div className="block">6</div>
         </div>
     </div>
   )
