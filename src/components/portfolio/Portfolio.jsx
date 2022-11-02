c2bb6558fd9a3429ec9f675b34cdd972ea0d118a
RaoulBumbasiJr <100057772+RaoulBumbasiJr@users.noreply.github.com>
2022-11-01 17:53:26 -0700

    Add files via upload

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,11 +4,51 @@ import "./portfolio.css"
 export default function portfolio() {
   return (
     <div className="portfolio" id="portfolio">
-        <div className="box">1</div>
-        <div className="box">2</div>
-        <div className="box">3</div>
-        <div className="box">4</div>
-
+          
+        <div className="box" >
+          <div className="linkContainer">
+            <a className="projectLink" 
+              href="https://www.youtube.com/" 
+              target="_blank" 
+              rel="noreferrer noopener">
+                personal website
+            </a>
+          </div>
+        </div>
+    
+        <div className="box" >
+          <div className="linkContainer">
+            <a className="projectLink" 
+              href="https://www.youtube.com/" 
+              target="_blank" 
+              rel="noreferrer noopener">
+                eCommerce website
+            </a>
+          </div>
+        </div>
+            
+        <div className="box" >
+          <div className="linkContainer">
+            <a className="projectLink" 
+              href="https://www.youtube.com/" 
+              target="_blank" 
+              rel="noreferrer noopener">
+                flashcard application
+            </a>
+          </div>
+        </div>
+            
+        <div className="box" >
+          <div className="linkContainer">
+            <a className="projectLink" 
+              href="https://www.youtube.com/" 
+              target="_blank" 
+              rel="noreferrer noopener">
+                api project
+            </a>
+          </div>
+        </div>
     </div>
   )
 }
+/*<a href="https://www.youtube.com/" className='linkButton'><div className="label">personal website</div></a>*/
--- src/components/quotes/Quotes.jsx
+++ src/components/quotes/Quotes.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
 import "./quotes.css"
+import gay from '../../images/gayNigga.png';
 
 export default function quotes() {
   return (
     <div className="quotes" id="quotes">
-        quotes
+        
+        <div className="proverbBox">
+          <div className="container">
+            <div className='holder'><img src={gay} alt="html" class="logo" className="gay"/></div>
+            <div className="proverb" id="iroh">"failure is only the oppurtuninty to begin again" - Iroh</div>
+          </div>
+        </div>
+        
+        <div className="proverbBox"> 
+          <div className="container">
+          <div className="proverb" id="buddha">"hapiness will never come to those who fail to appreciate what they already have" - Buddha</div>
+          </div>
+        </div>
+
+        
+        <div className="proverbBox">
+          <div className="container">
+          <div className="proverb" id="nagato">"feel the pain, think about pain, know the pain, and accept the pain" - Nagato</div>
+          </div>
+        </div>
+        
+        <div className="proverbBox">
+          <div className="container">
+          <div className="proverb" id="kaori">"someday when the pages of my life end, i know that you will be the one of the most beautiful chapters" -Kaori</div>
+          </div>
+        </div>
     </div>
   )
 }
