f9f0b95ff0d08378ed497512d4dc7f1c783ef9c8
RaoulBumbasiJr <100057772+RaoulBumbasiJr@users.noreply.github.com>
2022-11-02 21:14:44 -0700

    Add files via upload

--- src/components/quotes/Quotes.jsx
+++ src/components/quotes/Quotes.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import "./quotes.css"
-import gay from '../../images/gayNigga.png';
 
 export default function quotes() {
   return (
@@ -8,21 +7,20 @@ export default function quotes() {
         
         <div className="proverbBox">
           <div className="container">
-            <div className='holder'><img src={gay} alt="html" class="logo" className="gay"/></div>
-            <div className="proverb" id="iroh">"failure is only the oppurtuninty to begin again" - Iroh</div>
+            <div className="proverb" id="iroh">"failure is only the opportunity to begin again" - Iroh</div>
           </div>
         </div>
         
         <div className="proverbBox"> 
           <div className="container">
-          <div className="proverb" id="buddha">"hapiness will never come to those who fail to appreciate what they already have" - Buddha</div>
+          <div className="proverb" id="buddha">"happiness will never come to those who fail to appreciate what they already have" - Buddha</div>
           </div>
         </div>
 
         
         <div className="proverbBox">
           <div className="container">
-          <div className="proverb" id="nagato">"feel the pain, think about pain, know the pain, and accept the pain" - Nagato</div>
+          <div className="proverb" id="nagato">"people live their lives bound by what they accept as correct and true, that's how the define reality" - Itachi</div>
           </div>
         </div>
         
